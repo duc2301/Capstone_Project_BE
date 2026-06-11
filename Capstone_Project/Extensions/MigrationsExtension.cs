a980c7d37b345562a5bc55632b06ddf4117707a1
PainInVein <lesybinh2004@gmail.com>
2026-06-11 15:41:33 +0700

    system: Add auto migration whenever member run the code with new tables or first time pull

---
+++ Capstone_Project/Extensions/MigrationsExtension.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DbContexts;
+
+namespace Capstone_Project.Extensions
+{
+    public static class MigrationsExtension
+    {
+        public static IHost ApplyMigrations(this IHost host)
+        {
+            host.MigrateDatabase<CDESystemDbContext>();
+
+            return host;
+        }
+    }
+}
