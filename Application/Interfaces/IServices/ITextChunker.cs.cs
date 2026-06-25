1bf70cac79f7b419d0b130984fa4b36ca8c61324
duc2301 <phamvanduc2301@gmail.com>
2026-06-25 20:42:55 +0700

    feature: file text extractor

---
+++ Application/Interfaces/IServices/ITextChunker.cs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.IServices
+{
+    public interface ITextChunker
+    {
+        IReadOnlyList<string> Split(string text, int maxChars, int overlap);
+    }
+}
