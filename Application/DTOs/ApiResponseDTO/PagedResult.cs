81e7b20d5c1faebf30d0ed4db393f19ef632fc3c
PainInVein <lesybinh2004@gmail.com>
2026-08-22 22:49:12 +0700

    fix: add divide by 0 for pagination

--- Application/DTOs/ApiResponseDTO/PagedResult.cs
+++ Application/DTOs/ApiResponseDTO/PagedResult.cs
@@ -10,7 +10,7 @@ namespace Application.DTOs.ApiResponseDTO
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
 
-        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
         public bool HasNextPage => PageNumber < TotalPages;
         public bool HasPreviousPage => PageNumber > 1;
 
